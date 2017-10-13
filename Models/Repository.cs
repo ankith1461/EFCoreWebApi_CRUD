458d255185de20200b1629de449d8321a9c99829
Ankith Reddy <ankith_reddy146@hotmail.com>
2017-10-13 16:59:43 +0800

    Added New Repository Class for Test

---
+++ Models/Repository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace EFCoreWebApi_CRUD.Models{
+    public class Repository{
+        public List<Actor> Actors()
+        {
+            List<Actor> actors= new List<Actor>();
+            actors.Add(new Actor{Actor_ID=1,First_Name="PENELOPE",Last_Name="GUINESS"});
+            actors.Add(new Actor{Actor_ID=2,First_Name="NICK",Last_Name="WAHLBERG"});
+            actors.Add(new Actor{Actor_ID=3,First_Name="ED",Last_Name="CHASE"});
+            actors.Add(new Actor{Actor_ID=4,First_Name="JENNIFER",Last_Name="DAVIS"});
+
+            return actors;
+
+        }
+    }
+}
